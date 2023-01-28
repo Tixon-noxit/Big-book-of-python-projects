7c1d2cae37f5313ad02c7d1f81d2af4e6760825f
Tikhon Chabusov <77895701+Tixon-noxit@users.noreply.github.com>
2023-01-28 13:43:48 +0300

    Create README.py

---
+++ src/Bitmap/README.py
@@ -0,0 +1,7 @@
+# About this task
+
+In this program, a bitmap (bitmap) - a two-dimensional image, each pixel of which can be one of two colors, 
+is represented as a multi-line string value and serves to determine how the user's message is displayed. In our bitmap, 
+spaces correspond to white space, and all other characters are replaced by characters from the user message. 
+The provided bitmap resembles a map of the world, but you can replace it with any other you wish. 
+The simplicity of the "blank space or message characters" binary representation system makes it ideal for beginners.
